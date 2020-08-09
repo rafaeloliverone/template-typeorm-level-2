b9996a37ae66f7bea096ac17f67f38d957a9da3c
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-08-09 01:03:25 -0300

    router transaction post receiving the request data and sending it to the services

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -2,9 +2,21 @@
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
   }
 }
 
