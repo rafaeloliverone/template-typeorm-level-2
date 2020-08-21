2f40e72cb6e8f8c37cf4a9d7bf65513303362eaa
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-08-21 01:59:28 -0300

    deleting transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(idTransaction: string): Promise<void> {
+    const transactionRepository = await getCustomRepository(
+      TransactionsRepository,
+    );
+
+    const transaction = await transactionRepository.findOne(idTransaction);
+
+    if (transaction) {
+      await transactionRepository.delete({
+        id: idTransaction,
+      });
+    } else {
+      throw new AppError('Not found transaction with this id', 404);
+    }
   }
 }
 
