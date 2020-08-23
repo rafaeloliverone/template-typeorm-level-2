787ca909ca065272cc280d9798f86870877bb327
Rafael Oliveira <rafaelbatistacg@gmail.com>
2020-08-22 22:05:04 -0300

    adding cors and show transactions

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -14,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-
+app.use(cors());
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
